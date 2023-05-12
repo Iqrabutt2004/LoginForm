ded6c5487f23360f6b38fee2b661e95e0fe8f081
Iqra Butt <iqrabutt@MacBook-Pro.local>
2023-05-12 19:03:25 +0500

    code changes

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
+import "./form.css";
 
 const Form = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
-    contact:"",
+    contact: "",
   });
+
   const [tableData, setTableData] = useState([]);
 
-  useEffect(() => {
-    const storedFormData = JSON.parse(localStorage.getItem("formData"));
+useEffect(() => {
+  const storedFormData = JSON.parse(localStorage.getItem("formData"));
+//  executed after the component has rendered for the first time and after every update
 
-    if (storedFormData) {
-      setFormData(storedFormData);
-    }
+  if (storedFormData) { 
+    setFormData(storedFormData);
+  }
+  const storedTableData = JSON.parse(localStorage.getItem("tableData"));
 
-    const storedTableData = JSON.parse(localStorage.getItem("tableData"));
-    if (storedTableData) {
-      setTableData(storedTableData);
-    }
-  }, []);
+  if (storedTableData) {
+    setTableData(storedTableData);
+  }
+  setFormData({
+    name: "",
+    email: "",
+    password: "",
+    contact: "",
+  });
+}, []);
 
   const handleChange = (event) => {
     setFormData({
@@ -29,61 +39,79 @@ const Form = () => {
     });
   };
 
+  const handleUpdate = () => {
+    const updatedTableData = [...tableData]; //spread operator allows you to create a new array with all the elements of an existing array.
+    updatedTableData[tableData.length - 1] = formData;
+    localStorage.setItem("tableData", JSON.stringify(updatedTableData));
+    setTableData(updatedTableData);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    localStorage.removeItem("tableData");
-    setTableData([]);
-    setTableData([formData]);
-
+    const newTableData = [...tableData, formData];
+    localStorage.setItem("tableData", JSON.stringify(newTableData));
+    setTableData(newTableData);
     localStorage.setItem("formData", JSON.stringify(formData));
-    localStorage.setItem("tableData", JSON.stringify([formData]));
-
-    setFormData({
-      name: "",
-      email: "",
-      password: "",
-      contact: "",
-    });
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
+      <form
+        className="d-flex flex-column justify-content-center align-items-center"
+        onSubmit={handleSubmit}
+      >
+        <label className="d-flex text-font" htmlFor="name">
+          Name
+        </label>
         <input
+          className="input-fields"
           type="text"
           id="name"
           name="name"
           value={formData.name}
           onChange={handleChange}
         />
-        <label htmlFor="email">Email</label>
+        <label className="d-flex text-font" htmlFor="email">
+          Email
+        </label>
         <input
+          className="input-fields"
           type="email"
           id="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
         />
-        <label htmlFor="password">Password</label>
+        <label className="d-flex text-font" htmlFor="password">
+          Password
+        </label>
         <input
+          className="input-fields"
           type="password"
           id="password"
           name="password"
           value={formData.password}
           onChange={handleChange}
         />
-        <label htmlFor="contact">Contact</label>
+        <label className="d-flex text-font" htmlFor="contact">
+          Contact
+        </label>
         <input
+          className="input-fields"
           type="contact"
           id="contact"
           name="contact"
           value={formData.contact}
           onChange={handleChange}
         />
-        <button type="submit">Submit</button>
+        <button className=" my-3 btn-padding" type="submit">
+          Submit
+        </button>
+        <button className="btn-padding" type="button" onClick={handleUpdate}>
+          Update
+        </button>
       </form>
-      <table>
+      <Table>
         <thead>
           <tr>
             <th>Name</th>
@@ -93,16 +121,16 @@ const Form = () => {
           </tr>
         </thead>
         <tbody>
-          {tableData.map((data, index) => (
-            <tr key={index}>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.password}</td>
-              <td>{data.contact}</td>
+          {tableData.length > 0 && (
+            <tr>
+              <td>{tableData[tableData.length - 1].name}</td>
+              <td>{tableData[tableData.length - 1].email}</td>
+              <td>{tableData[tableData.length - 1].password}</td>
+              <td>{tableData[tableData.length - 1].contact}</td>
             </tr>
-          ))}
+          )}
         </tbody>
-      </table>
+      </Table>
     </>
   );
 };
